7c93bd42dee3c31a5f4c00e7c053a91dcdf432be
gaoyujin <free_sky2007@aliyun.com>
2024-08-19 23:16:02 +0800

    项目初始化

---
+++ src/main/java/com/gaoyj/paymentdemo/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.gaoyj.paymentdemo.controller;
+
+import com.gaoyj.paymentdemo.vo.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@Api(tags="商品管理")
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @ApiOperation("测试接口")
+    @GetMapping("/test")
+    public R test() {
+        return R.ok().data("message","test hello").data("now",new Date());
+    }
+}
